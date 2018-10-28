4bea608ebf495cdbb505585977ea32d035ed9eb3
Zack <43247450+ziqiangt@users.noreply.github.com>
2018-10-28 11:49:44 -0700

    add 7-segment LEDs and update the FSM

--- claw_machine.sv
+++ claw_machine.sv
@@ -1,17 +1,22 @@
-module claw_machine(rstn, clk, one, two, catch, refund, strength, balance);
+module claw_machine(rstn, clk, enable, one, two, catch, refund, strength, balance, strength_LED, balance_units_LED, balance_tens_LED, max_count);
 
 //I/O ports
 input  logic		rstn;
+input  logic		enable;
 input  logic		clk;
 input  logic		one;
 input  logic		two;
 input  logic		catch;
 input  logic		refund;		
-output logic 	[3:0]	strength;	//up to 9 strength
-output logic 	[3:0]	balance;	//up tp 12 dollars refund
+output logic 	[3:0]	strength;		//up to 9 strength
+output logic 	[3:0]	balance;		//up tp 14 dollars refund: state 11 + 3 dollars input
+output logic 	[6:0]	strength_LED;		//7 segment LED to display strength
+output logic 	[6:0]	balance_units_LED;	//7 segment LED to display the balance units
+output logic 	[6:0]	balance_tens_LED;	//7 segment LED to display the tens units
+output logic 	[31:0]	max_count;		//counter
 
 //FSM
-typedef enum{IDEL, S1, S2, S3, S4, S5, S6, S7, S8, S9} fsmstate_e;	//10 states
+typedef enum{IDEL, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11} fsmstate_e;	//12 states
 
 fsmstate_e cstate;	//current state
 fsmstate_e nstate;	//next state
@@ -23,6 +28,9 @@ wire 			C3;
 wire 			C4;
 wire 			C5;
 
+//internal counters
+logic [31:0]	inter_counter;	
+
 //branch conditions
 assign C1 = (one && !two); 				//one dollar
 assign C2 = (!one && two);				//two dollars
@@ -31,15 +39,20 @@ assign C4 = (catch && !refund && !one && !two);		//catch
 assign C5 = (!catch && refund && !one && !two);		//refund
 
 //state transfer - sequencial logic
-always_ff @(posedge clk)
+always_ff @(posedge clk or negedge rstn)
 begin
 	if(~rstn)
 	begin
 		cstate 	 <= IDEL;
+		inter_counter <= 32'h00000000;
 	end
-	else
+	else if(enable)
 	begin
-		cstate <= nstate;
+		cstate 	 <= nstate;
+		if(C1 || C2 || C3 || C4 || C5) 
+		begin
+			inter_counter <= inter_counter + 1;
+		end
 	end
 end
 
@@ -119,7 +132,7 @@ begin
 		S7:begin
 			if(C1)		begin	nstate = S8;	end
 			else if(C2)	begin	nstate = S9;	end 
-			else if(C3)	begin	nstate = IDEL;	end
+			else if(C3)	begin	nstate = S10;	end
 			else if(C4)	begin	nstate = IDEL;	end
 			else if(C5)	begin	nstate = IDEL;	end
 			else		begin	nstate = S7;	end
@@ -128,8 +141,8 @@ begin
 		//S8 state
 		S8:begin
 			if(C1)		begin	nstate = S9;	end
-			else if(C2)	begin	nstate = IDEL;	end 
-			else if(C3)	begin	nstate = IDEL;	end
+			else if(C2)	begin	nstate = S10;	end 
+			else if(C3)	begin	nstate = S11;	end
 			else if(C4)	begin	nstate = IDEL;	end
 			else if(C5)	begin	nstate = IDEL;	end
 			else		begin	nstate = S8;	end
@@ -144,6 +157,26 @@ begin
 			else if(C5)	begin	nstate = IDEL;	end
 			else		begin	nstate = S9;	end
 		end
+		
+		//S10 state
+		S10:begin
+			if(C1)		begin	nstate = IDEL;	end
+			else if(C2)	begin	nstate = IDEL;	end 
+			else if(C3)	begin	nstate = IDEL;	end
+			else if(C4)	begin	nstate = IDEL;	end
+			else if(C5)	begin	nstate = IDEL;	end
+			else		begin	nstate = S10;	end
+		end
+		
+		//S11 state
+		S11:begin
+			if(C1)		begin	nstate = IDEL;	end
+			else if(C2)	begin	nstate = IDEL;	end 
+			else if(C3)	begin	nstate = IDEL;	end
+			else if(C4)	begin	nstate = IDEL;	end
+			else if(C5)	begin	nstate = IDEL;	end
+			else		begin	nstate = S11;	end
+		end
 	
 		default:begin
 				nstate = IDEL;
@@ -151,53 +184,28 @@ begin
 			
 	endcase
 end
+
 //branch output - combinational logic
 always_comb
 begin
 	case(cstate)
 		//IDEL state
 		IDEL:begin
-			if(C1)
-			begin
+				strength_LED =       7'b1111110;	//seven-segment LED display decimal 0 
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0 
+				balance_units_LED =  7'b1111110;	//seven-segment LED display decimal 0 
 				strength = 4'b0000;
 				balance = 4'b0000;
-			end
-			else if(C2)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end 
-			else if(C3)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
 		end
 
 
 		//S1 state
 		S1:begin
-			if(C1)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C2)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end 
-			else if(C3)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C4)
+				strength_LED =       7'b0110000;	//seven-segment LED display decimal 1 
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0 
+				balance_units_LED =  7'b0110000;	//seven-segment LED display decimal 1 
+
+			if(C4)
 			begin
 				strength = 4'b0001;	//catch with strength level 1
 				balance = 4'b0000;
@@ -216,22 +224,10 @@ begin
 
 		//S2 state
 		S2:begin
-			if(C1)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C2)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end 
-			else if(C3)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C4)
+				strength_LED =       7'b1101101;	//seven-segment LED display decimal 2
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0 
+				balance_units_LED =  7'b1101101;	//seven-segment LED display decimal 2 
+			if(C4)
 			begin
 				strength = 4'b0010;	//catch with strength level 2
 				balance = 4'b0000;
@@ -251,22 +247,10 @@ begin
 
 		//S3 state
 		S3:begin
-			if(C1)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C2)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end 
-			else if(C3)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C4)
+				strength_LED =       7'b1111001;	//seven-segment LED display decimal 3
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0 
+				balance_units_LED =  7'b1111001;	//seven-segment LED display decimal 3 
+			if(C4)
 			begin
 				strength = 4'b0011;	//catch with strength level 3
 				balance = 4'b0000;
@@ -285,22 +269,10 @@ begin
 
 		//S4 state
 		S4:begin
-			if(C1)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C2)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end 
-			else if(C3)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C4)
+				strength_LED =  7'b0110011;		//seven-segment LED display decimal 4
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0 
+				balance_units_LED =  7'b0110011;	//seven-segment LED display decimal 4 
+			if(C4)
 			begin
 				strength = 4'b0100;	//catch with strength level 4
 				balance = 4'b0000;
@@ -319,22 +291,10 @@ begin
 
 		//S5 state
 		S5:begin
-			if(C1)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C2)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end 
-			else if(C3)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C4)
+				strength_LED = 	     7'b1011011;	//seven-segment LED display decimal 5 
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0
+				balance_units_LED =  7'b1011011;	//seven-segment LED display decimal 5
+			if(C4)
 			begin
 				strength = 4'b0101;	//catch with strength level 5
 				balance = 4'b0000;
@@ -353,22 +313,10 @@ begin
 
 		//S6 state
 		S6:begin
-			if(C1)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C2)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end 
-			else if(C3)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C4)
+				strength_LED =       7'b1011111;	//seven-segment LED display decimal 6
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0
+				balance_units_LED =  7'b1011111;	//seven-segment LED display decimal 6
+			if(C4)
 			begin
 				strength = 4'b0110;	//catch with strength level 6
 				balance = 4'b0000;
@@ -386,22 +334,10 @@ begin
 		end
 		//S7 state
 		S7:begin
-			if(C1)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C2)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end 
-			else if(C3)
-			begin
-				strength = 4'b0000;
-				balance = 4'b1010;	//over the limits, refund all money: 10 dollars
-			end
-			else if(C4)
+				strength_LED =       7'b1110000;	//seven-segment LED display decimal 7
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0
+				balance_units_LED =  7'b1110000;	//seven-segment LED display decimal 7
+			if(C4)
 			begin
 				strength = 4'b0111;	//catch with strength level 7
 				balance = 4'b0000;
@@ -420,22 +356,10 @@ begin
 
 		//S8 state
 		S8:begin
-			if(C1)
-			begin
-				strength = 4'b0000;
-				balance = 4'b0000;
-			end
-			else if(C2)
-			begin
-				strength = 4'b0000;
-				balance = 4'b1010;	//over the limits, refund all money: 10 dollars
-			end 
-			else if(C3)
-			begin
-				strength = 4'b0000;
-				balance = 4'b1011;	//over the limits, refund all money: 11 dollars
-			end
-			else if(C4)
+				strength_LED =       7'b1111111;	//seven-segment LED display decimal 8
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0
+				balance_units_LED =  7'b1111111;	//seven-segment LED display decimal 8
+			if(C4)
 			begin
 				strength = 4'b1000;	//catch with strength level 8
 				balance = 4'b0000;
@@ -455,6 +379,9 @@ begin
 
 		//S9 state
 		S9:begin
+				strength_LED =       7'b1111011;	//seven-segment LED display decimal 9
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0
+				balance_units_LED =  7'b1111011;	//seven-segment LED display decimal 9
 			if(C1)
 			begin
 				strength = 4'b0000;
@@ -486,12 +413,92 @@ begin
 				balance = 4'b0000;
 			end
 		end
-	
+
+		//S10 state
+		S10:begin	
+				strength_LED =       7'b1111011;	//seven-segment LED display decimal 9
+				balance_tens_LED  =  7'b0110000;	//seven-segment LED display decimal 1
+				balance_units_LED =  7'b1111110;	//seven-segment LED display decimal 0
+			if(C1)
+			begin
+				strength = 4'b0000;
+				balance = 4'b1011;	//over limits, refund all money: 11 dollars 	
+			end
+			else if(C2)
+			begin
+				strength = 4'b0000;
+				balance = 4'b1100;	//over limits, refund all money: 12 dollars
+			end 
+			else if(C3)
+			begin
+				strength = 4'b0000;
+				balance = 4'b1101;	//over the limits, refund all money: 13 dollars
+			end
+			else if(C4)
+			begin
+				strength = 4'b1001;	//catch with strength level 9
+				balance = 4'b0001;	//extra 1 dollar
+			end
+			else if(C5)
+			begin
+				strength = 4'b0000;	
+				balance = 4'b1010;	//refund money 10 dollars
+			end
+			else
+			begin
+				strength = 4'b0000;
+				balance = 4'b0000;
+			end
+		end	
+
+		//S11 state
+		S11:begin
+				strength_LED =       7'b1111011;	//seven-segment LED display decimal 9
+				balance_tens_LED  =  7'b0110000;	//seven-segment LED display decimal 1
+				balance_units_LED =  7'b0110000;	//seven-segment LED display decimal 1
+			if(C1)
+			begin
+				strength = 4'b0000;
+				balance = 4'b1100;	//over limits, refund all money: 12 dollars 	
+			end
+			else if(C2)
+			begin
+				strength = 4'b0000;
+				balance = 4'b1101;	//over limits, refund all money: 13 dollars
+			end 
+			else if(C3)
+			begin
+				strength = 4'b0000;
+				balance = 4'b1110;	//over the limits, refund all money: 14 dollars
+			end
+			else if(C4)
+			begin
+				strength = 4'b1001;	//catch with strength level 9
+				balance = 4'b0010;	//extra 2 dollars
+			end
+			else if(C5)
+			begin
+				strength = 4'b0000;	
+				balance = 4'b1011;	//refund money 11 dollars
+			end
+			else
+			begin
+				strength = 4'b0000;
+				balance = 4'b0000;
+			end
+		end
+
 		default:begin
 				strength = 4'b0000;
 				balance = 4'b0000;
+				strength_LED =  7'b1111110;		//seven-segment LED display decimal 0
+				balance_tens_LED  =  7'b1111110;	//seven-segment LED display decimal 0 
+				balance_units_LED =  7'b1111110;	//seven-segment LED display decimal 0 
 		end
 			
 	endcase
 end
+
+assign max_count = inter_counter;
+
 endmodule
